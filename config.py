a93d1473f002ba37c61298b2552d7825920e3c8d
Highpower143 <nandyadu1c@gmail.com>
2025-01-09 16:17:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 29893020))
-API_HASH = getenv("API_HASH", "28e79037f0b334ef0503466c53f08af5")
+API_ID = int(getenv("API_ID", 27572631))
+API_HASH = getenv("API_HASH", "bf8945f526eca72cf810474e451da661")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", 6399386263))
+OWNER_ID = int(getenv("OWNER_ID", 1786683163))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Blissfull_Music")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "akaChampu")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "itsMeShivanshu")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "NYCreation_Chatzone")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "Creative Ydv")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "TMzeroo")
 
 # Random Start Images
 IMG = [
